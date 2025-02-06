5e42a75fbc440fdf58608578e2936bf353811bb8
TUCCHI1 <tutikikin1@gmail.com>
2025-02-06 16:42:18 +0900

    feat(state-machine): add layout annotation to toggle state machine

--- src/state-machine.ts
+++ src/state-machine.ts
@@ -1,6 +1,7 @@
 import { setup } from "xstate";
 
 const toggleMachine = setup({}).createMachine({
+	/** @xstate-layout N4IgpgJg5mDOIC5QBcD2UoBswDoCSAdgIYDGyAlgG5gDEaG2A2gAwC6ioADqrORagQ4gAHogCMAVgDsOAMwBOZgDZZYsfIBMSqQBYNGgDQgAnuNU4xOgBw7ZGnRMVLJAXxdH6WXAEEyVWp5MbELcvPyCSCLi0nJOqupauvpGpghWYnL68mKyOvJWEpaqbu4gBKgQcEKBYCE8fOQCQqIIALRKKYjtbh7oXvjEftR1YY0RoC16nWkyzFL2UtlKGmIasjY9IDU4vhTDkaENTZEtWhJyBbZ6EhJWqh0miFbyONILSytrVkolLkA */
 	id: "toggle",
 	initial: "Inactive",
 	states: {
